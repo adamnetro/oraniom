9b387e8752cf9ed5097f7c643af0eef7dd64e696
adamnetro <64706091+adamnetro@users.noreply.github.com>
2024-02-08 22:04:23 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+let toRts = document.querySelectorAll(".toRight");
+let toLfs = document.querySelectorAll(".toLeft");
+
+function lang() {
+  if (localStorage.getItem("ar")) {
+    document.querySelector('.ar').click()
+  }
+}
+lang();
+
 function chgEn() {
   location.reload();
+  localStorage.clear();
 }
 
 function chgAr(e) {
@@ -10,4 +21,31 @@ function chgAr(e) {
     lang.textContent = lang.getAttribute("data-lang");
   });
   document.querySelector("div h1").innerHTML += ` <span>.com</span>`;
+  toRts.forEach((toRt) => {
+    toRt.classList.replace("toRight", "toLeft");
+  });
+  toLfs.forEach((toLf) => {
+    toLf.classList.replace("toLeft", "toRight");
+  });
+
+  localStorage.setItem("ar", "On");
 }
+
+function scrollDown() {
+  window.scrollTo({
+    top: 477,
+  });
+}
+
+window.onscroll = function () {
+  toRts.forEach((toRt) => {
+    if (window.scrollY >= toRt.offsetTop - 500) {
+      toRt.classList.add("toRightOn");
+    }
+  });
+  toLfs.forEach((toLf) => {
+    if (window.scrollY >= toLf.offsetTop - 500) {
+      toLf.classList.add("toLeftOn");
+    }
+  });
+};
